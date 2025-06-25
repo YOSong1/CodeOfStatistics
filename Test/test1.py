803f9e536a5d691a828816165ab097d4fe53e3e4
Song Young Ohk <43607595+YOSong1@users.noreply.github.com>
2025-06-25 23:54:50 +0900

    Create test1.py

---
+++ Test/test1.py
@@ -0,0 +1,115 @@
+word_list = ['This', 'book', 'is', 'designed', 'specifically', 
+    'with', 'programming', 'beginners', 'in', 'mind', 'Basic', 
+    'programming', 'concepts', 'such', 'as', 'expressions', 
+    'variables', 'loops', 'decisions', 'lists', 'dictionaries', 
+    'sets', 'functions', 'files', 'classes', 'objects', 'SQLite', 
+    'databases', 'regular', 'expressions', 'and', 'graphical', 
+    'interfaces', 'with', 'Tkinter', 'are', 'clearly', 
+    'presented', 'one', 'by', 'one', 'with', 'examples', 
+    'exercises', 'illustrate', 'The', 'book', 'aims',  
+    'explore', 'computer', 'programming', 'as', 'an', 
+    'everyday', 'tool', 'It', 'can', 'be', 'read', 'Powerful', 'language',
+    'during', 'an', 'introduction', 'to', 'computer', 'Powerful', 'language',
+    'programming', 'course', 'or', 'used', 'as', 'learn', 'programming',
+    'study', 'guide', 'for', 'self', 'learners', 'Powerful', 'language',
+    'Only', 'basic', 'computer', 'knowledge', 'such', 'as', 'Powerful', 'language', 
+    'typing', 'texts', 'opening', 'and', 'saving', 'files', 'is', 
+    'needed', 'to', 'start', 'the', 'course', 'All', 'software', 
+    'used', 'in', 'the', 'book', 'can', 'be', 'downloaded', 'for', 'free', 
+    'runs', 'on', 'Windows', 'Linux', 'and', 'macOS', 'h', 'Python', 'version', 
+    'very', 'powerful', 'language', 'full', 'of', 'modern', 'programming', 'features', 
+    'this', 'book', 'focuses', 'primarily', 'on', 'programming', 'logic', 
+    'teaching', 'how', 'program', 'Some', 'language', 'features', 'were', 
+    'not', 'used', 'in', 'order', 'favor', 'programming', 'logic', 'exercises', 
+    'offer', 'the', 'reader', 'broader', 'preparation', 'for', 'other', 'languages', 
+    'Powerful', 'language', 'features', 'such', 'as', 'generators', 'sets', 'lambda', 
+    'functions', 'and', 'comprehensions', 'are', 'presented', 'and', 'the', 
+    'reader', 'will', 'find', 'various', 'notes', 'and', 'explanations', 'of', 'Python', 
+    'specific', 'features', 'in', 'addition', 'to', 'programming', 'logic', 'Who', 
+    'this', 'book', 'is', 'written', 'for', 'Students', 'who', 'want',  
+    'learn', 'programming', 'on', 'their', 'own', 'the', 'book', 'was', 'written', 
+    'so', 'you', 'can', 'gradually', 'learn', 'program', 'The', 'first', 'learn', 'programming',
+    'chapters', 'explain', 'the', 'basic', 'programming', 'concepts', 'in', 
+    'logical', 'order', 'with', 'practical', 'exercises', 'All', 'you', 'need', 
+    'to', 'know', 'is', 'how', 'use', 'computer', 'browser', 'Firefox', 
+    'Edge', 'Safari', 'or', 'Google', 'Chrome', 'The', 'book', 'starts', 'by', 'showing', 
+    'how', 'to', 'download', 'install', 'Python', 'on', 'Windows', 'but', 'it', 'also', 
+    'covers', 'Linux', 'Ubuntu', 'and', 'macOS', 'Special', 'attention', 'was', 'given', 
+    'to', 'conditional', 'structures', 'if', 'loops', 'while', 'functions', 'including', 
+    'recursion', 'The', 'book', 'also', 'covers', 'string', 'manipulation', 'text', 
+    'processing', 'so', 'you', 'learn', 'work', 'with', 'variables', 'containing', 
+    'text', 'prepare', 'to', 'write', 'more', 'complete', 'programs', 'At', 'the', 
+    'end', 'we', 'cover', 'files', 'creating', 'simple', 'web', 'pages', 'light', 
+    'introduction', 'to', 'object', 'oriented', 'programming', 'regular', 'expressions', 
+    'graphical', 'interfaces','string','structures','students','study','teaching','texts',
+    'their','tkinter','tool','typing','ubuntu','use','various','version','very','want','we',
+    'web','were','while','will','work','write','their','tkinter','tool','typing','ubuntu',
+    'use','various','version','very','want','we','web','were','while','will','work','write',
+    'tkinter',  'tool','typing','ubuntu','use',  'various',  'version', 'very', 'want',  'we', 
+    'web', 'were', 'while', 'will', 'work', 'write']
+
+
+
+
+
+
+
+# 다음과 같은 출력 결과가 나오도록 코딩하세요.
+# 어별 빈도수:
+# programming: 13
+# the: 11
+# book: 8
+# to: 8
+# and: 7
+# language: 7
+# powerful: 6
+# as: 5
+# in: 5
+# learn: 5
+# with: 5
+# computer: 4
+# features: 4
+# for: 4
+# is: 4
+# on: 4
+# tkinter: 4
+# tool: 4
+# typing: 4
+# ubuntu: 4
+# use: 4
+# various: 4
+# version: 4
+# very: 4
+# want: 4
+# we: 4
+# web: 4
+# were: 4
+# while: 4
+# will: 4
+# work: 4
+# write: 4
+# basic: 3
+#...
+# specific: 1
+# specifically: 1
+# sqlite: 1
+# start: 1
+# starts: 1
+# sqlite: 1
+# start: 1
+# starts: 1
+# sqlite: 1
+# start: 1
+# sqlite: 1
+# sqlite: 1
+# start: 1
+# sqlite: 1
+# sqlite: 1
+# start: 1
+# starts: 1
+
+# 가장 많이 나온 단어: 'programming' (총 13회)
+
+
+
+
